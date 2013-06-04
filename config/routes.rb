2ff499fb0e48e50c07630bbe660539677d941175
張 東哲 <youseke@chou-higashiakira-no-MacBook-Pro.local>
2013-06-04 22:12:24 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,20 @@
 SampleApp::Application.routes.draw do
+  get "users/new"
+
+  get "static_pages/home"
+
+  get "static_pages/help"
+
+  get "static_pages/about"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
+
+  match '/home', to:'static_pages#home'
+  match '/about', to:'static_pages#about'
+  match '/help', to:'static_pages#help'
+  match '/contact', to:'static_pages#contact'
+  match '/register', to:'users#new'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
